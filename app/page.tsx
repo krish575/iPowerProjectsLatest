97b9814af4909a4b8c7e69d7f174fa75426c6c6b
krish575 <krishpat2016@gmail.com>
2025-04-20 13:27:05 +0530

    Latest

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import ContactUs from "./components/ContactUs";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <ContactUs/>
+    </div>
+  );
+}
